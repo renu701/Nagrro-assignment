68ce3771d24e51a34b3f624dc69dd6628b509d3f
Renu <apple@Apples-MacBook-Pro.local>
2024-02-09 21:57:16 +0530

    finished functionality

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-// Import stylesheets
-import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function removeStructure(btn) {
+    let parentDiv = btn.parentNode;
+    while (parentDiv && !parentDiv.classList.contains('list-items')){
+        parentDiv = parentDiv.parentNode;
+    }
+    if(parentDiv) {
+        parentDiv.remove();
+    }
+    console.log("delete me",btn);
+}
+
+function toggle(event) {
+    const inputContainer = document.getElementById('inputForm');
+    console.log(event.textContent,inputContainer);
+    if(event.textContent === 'Hide') {
+        event.textContent = 'Show';
+        inputContainer.style.display = 'none';
+    }else {
+        event.textContent = 'Hide';
+        inputContainer.style.display = 'grid';
+    }
+}
+
+function getValue(event) {
+    console.log("dfghjkl",event.value);
+    const modal = document.getElementById('statusModal');
+
+    if(event.value === 'done'){
+        modal.style.display='block';
+    }
+}
+
+function hideModal(){
+    const modal = document.getElementById('statusModal');
+    modal.style.display='none';
+}
+
+function showLoader(){
+    const loading = document.getElementById("loading");
+    loading.style.display="block";
+    setTimeout(hideLoader, 2000);
+}
+function hideLoader() {
+    const loading = document.getElementById("loading");
+    loading.style.display="none";
+    window.location.href = '/thanks.html'
+
+}
